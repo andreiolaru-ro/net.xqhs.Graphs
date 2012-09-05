ecf16463e02b6b5ab67c8f3adc037a6f13091e96
andreiolaru <cs@andreiolaru.ro>
2012-09-05 15:06:26 +0300

    starting to fix errors and convert to Java 7.

---
+++ src/representation/RepresentationElement.java
@@ -0,0 +1,30 @@
+package representation;
+
+import graph.GraphComponent;
+
+public abstract class RepresentationElement
+{
+	public static class RepElementConfig extends Config
+	{
+		GraphRepresentation rootRepresentation;
+		GraphComponent	  representedComponent = null;
+		
+		public RepElementConfig(GraphRepresentation root, GraphComponent component)
+		{
+			this.rootRepresentation = root;
+			this.representedComponent = component;
+		}
+	}
+	
+	protected RepElementConfig config;
+	
+	public RepresentationElement(RepElementConfig conf)
+	{
+		this.config = conf;
+	}
+	
+	public GraphRepresentation getRootRepresentation()
+	{
+		return config.rootRepresentation;
+	}
+}
