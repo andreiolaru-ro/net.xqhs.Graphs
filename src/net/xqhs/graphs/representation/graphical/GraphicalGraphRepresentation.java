c373fc6d68d062a9d91571d51dae93e1d54f2f8a
andreiolaru <cs@andreiolaru.ro>
2013-06-26 15:16:48 +0300

    Solved more compilation errors.

---
+++ src/net/xqhs/graphs/representation/graphical/GraphicalGraphRepresentation.java
@@ -0,0 +1,270 @@
+/*******************************************************************************
+ * Copyright (C) 2013 Andrei Olaru.
+ * 
+ * This file is part of net.xqhs.Graphs.
+ * 
+ * net.xqhs.Graphs is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * net.xqhs.Graphs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with net.xqhs.Graphs.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.xqhs.graphs.representation.graphical;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import net.xqhs.graphical.GCanvas;
+import net.xqhs.graphical.GConnector;
+import net.xqhs.graphical.GContainer;
+import net.xqhs.graphs.graph.Edge;
+import net.xqhs.graphs.graph.Graph;
+import net.xqhs.graphs.representation.VisualizableGraphComponent;
+import net.xqhs.graphs.representation.graphical.GraphicalRepresentationElement.EdgeType;
+import net.xqhs.graphs.representation.graphical.GraphicalRepresentationElement.Type;
+import net.xqhs.graphs.representation.linear.LinearGraphRepresentation;
+
+public class GraphicalGraphRepresentation extends LinearGraphRepresentation
+{
+	GCanvas	canvas;
+	Point2D	topleft;
+	Point2D	bottomright;
+	
+	public GraphicalGraphRepresentation(Graph theGraph)
+	{
+		super(theGraph);
+	}
+	
+	@Override
+	public GraphicalGraphRepresentation makeDefaults()
+	{
+		super.makeDefaults();
+		setCanvas(new GCanvas());
+		topleft = new Point2D.Float(-100, -100);
+		bottomright = new Point2D.Float(100, 100);
+		return this;
+	}
+	
+	public GraphicalGraphRepresentation setCanvas(GCanvas canvas)
+	{
+		this.canvas = canvas;
+		return this;
+	}
+	
+	/**
+	 * The origin of the rectangle for this representation, on the GCanvas
+	 */
+	public GraphicalGraphRepresentation setOrigin(Point2D origin)
+	{
+		this.topleft = origin;
+		return this;
+	}
+	
+	public GraphicalGraphRepresentation setBottomRight(Point2D bottomRight)
+	{
+		this.bottomright = bottomRight;
+		return this;
+	}
+	
+	@Override
+	protected String setDefaultName(String name)
+	{
+		return super.setDefaultName(name) + "X";
+	}
+	
+	@Override
+	protected void processGraph()
+	{
+		super.processGraph(); // calculates paths
+		
+		Set<PathElement> blackNodes = new HashSet<PathElement>();
+		GraphicalRepresentationElement representation = new GraphicalRepresentationElement(this, null,
+				Type.ELEMENT_CONTAINER);
+		
+		for(PathElement el : paths)
+			if(!blackNodes.contains(el))
+				representation.connected.add(representChildren(el, blackNodes));
+		
+		this.theRepresentation = representation;
+		
+		doLayout();
+		lf("get ready for magnetics");
+		canvas.startMagnetics();
+	}
+	
+	protected GraphicalRepresentationElement representChildren(PathElement el, Set<PathElement> blackNodes)
+	{
+		// FIXME: check casts to VisualizableGraphComponent throughout the method
+		GraphicalRepresentationElement repr = new GraphicalRepresentationElement(this,
+				(VisualizableGraphComponent) el.getNode(), Type.NODE);
+		blackNodes.add(el);
+		
+		List<PathElement> others = new LinkedList<PathElement>(el.getOtherChildren());
+		
+		representOthers(others, blackNodes, EdgeType.BACKLINK, el, repr);
+		
+		boolean first = true;
+		for(PathElement child : el.getChildren())
+		{
+			Edge edge = null;
+			if(isBackwards)
+				edge = el.getNode().getEdgesFrom(child.getNode()).iterator().next();
+			else
+				edge = el.getNode().getEdgesTo(child.getNode()).iterator().next();
+			GraphicalRepresentationElement childRepr = representChildren(child, blackNodes);
+			GraphicalRepresentationElement edgeRepr = new GraphicalRepresentationElement(this,
+					(VisualizableGraphComponent) edge, Type.EDGE);
+			edgeRepr.setEdge(EdgeType.CHILDLINK, repr, childRepr);
+			
+			representOthers(others, blackNodes, first ? EdgeType.FORELINK : EdgeType.SIDELINK, el, repr);
+			
+			repr.connected.add(edgeRepr);
+			((VisualizableGraphComponent) child.getNode()).addRepresentation(childRepr);
+			((VisualizableGraphComponent) edge).addRepresentation(edgeRepr);
+			first = false;
+		}
+		representOthers(others, blackNodes, EdgeType.EXTLINK, el, repr);
+		
+		return repr;
+	}
+	
+	protected void representOthers(List<PathElement> others, Set<PathElement> blackNodes, EdgeType edgeType,
+			PathElement parent, GraphicalRepresentationElement parentRepr)
+	{
+		// FIXME: check casts to VisualizableGraphComponent throughout the method
+		while(!others.isEmpty() && ((edgeType == EdgeType.EXTLINK) || blackNodes.contains(others.get(0))))
+		{
+			PathElement other = others.get(0);
+			EdgeType actualEdgeType = edgeType;
+			GraphicalRepresentationElement otherRepr = (GraphicalRepresentationElement) ((VisualizableGraphComponent) other
+					.getNode()).getFirstRepresentationForPlatform(parentRepr.getRootRepresentation());
+			if(otherRepr == null)
+			{
+				others.remove(other);
+				continue;
+				// FIXME this may happen for some SIDELINKS. decide.
+			}
+			
+			if((actualEdgeType == EdgeType.BACKLINK) && !parent.pathContains(other))
+				actualEdgeType = EdgeType.SIDELINK;
+			Edge edge = null;
+			if(isBackwards)
+				edge = parent.getNode().getEdgesFrom(other.getNode()).iterator().next();
+			else
+				edge = parent.getNode().getEdgesTo(other.getNode()).iterator().next();
+			GraphicalRepresentationElement edgeRepr = new GraphicalRepresentationElement(this,
+					(VisualizableGraphComponent) edge, Type.EDGE);
+			edgeRepr.setEdge(actualEdgeType, parentRepr, otherRepr);
+			
+			parentRepr.connected.add(edgeRepr);
+			((VisualizableGraphComponent) edge).addRepresentation(edgeRepr);
+			others.remove(other);
+		}
+	}
+	
+	protected void doLayout()
+	{
+		float w = (float) (bottomright.getX() - topleft.getX());
+		float h = (float) (bottomright.getY() - topleft.getY());
+		
+		Point measurement = measureLayout((GraphicalRepresentationElement) theRepresentation);
+		int wc = measurement.x;
+		int hc = measurement.y;
+		
+		GraphicalRepresentationElement repr = (GraphicalRepresentationElement) theRepresentation;
+		((GContainer) repr.gelement).setSize(w, h);
+		doLayout((GraphicalRepresentationElement) theRepresentation, new Point(1, 0), w / (wc + 2), h / (hc + 1), repr);
+		
+	}
+	
+	protected void doLayout(GraphicalRepresentationElement repr, Point cPos, float wFactor, float hFactor,
+			GraphicalRepresentationElement container)
+	{
+		repr.gelement.setCanvas(canvas);
+		if(repr.glabel != null)
+			repr.glabel.setCanvas(canvas);
+		if(repr.type == Type.NODE)
+			((GContainer) container.gelement).addReferencingElement(repr.gelement);
+		
+		li("layout for: [" + repr.label + "] at [" + cPos + "] having size [" + repr.subSize + "] : "
+				+ (repr.edgeType == null ? "-" : repr.edgeType));
+		switch(repr.type)
+		{
+		case EDGE:
+			if(repr.edgeType == EdgeType.CHILDLINK)
+				doLayout(repr.connected.get(1), new Point(cPos), wFactor, hFactor, container);
+			((GConnector) repr.gelement).setFrom(repr.connected.get(0).gelement).setTo(repr.connected.get(1).gelement);
+			break;
+		case NODE:
+			repr.positionInGrid(new Point(cPos), hFactor, hFactor);
+			cPos.setLocation(cPos.x, cPos.y + 1);
+			for(GraphicalRepresentationElement sub : repr.connected)
+			{
+				doLayout(sub, new Point(cPos), wFactor, hFactor, container);
+				if(sub.edgeType == EdgeType.CHILDLINK)
+					cPos.setLocation(cPos.x + sub.subSize.x, cPos.y);
+			}
+			break;
+		case ELEMENT_CONTAINER:
+			cPos.setLocation(cPos.x, cPos.y + 1);
+			for(GraphicalRepresentationElement sub : repr.connected)
+			{
+				doLayout(sub, new Point(cPos), wFactor, hFactor, container);
+				cPos.setLocation(cPos.x + sub.subSize.x, cPos.y);
+			}
+		}
+	}
+	
+	protected Point measureLayout(GraphicalRepresentationElement repr)
+	{
+		int height = 0;
+		int width = 0;
+		switch(repr.type)
+		{
+		case ELEMENT_CONTAINER:
+			for(GraphicalRepresentationElement sub : repr.connected)
+			{
+				Point result = measureLayout(sub);
+				if(result.y > height)
+					height = result.y;
+				width += result.x;
+			}
+			repr.setSize(new Point(width, height));
+			return new Point(width, height);
+		case EDGE:
+			return repr.setSize(measureLayout(repr.connected.get(1))).subSize;
+		case NODE:
+			for(GraphicalRepresentationElement sub : repr.connected)
+			{
+				if(sub.edgeType != EdgeType.CHILDLINK)
+					continue;
+				Point result = measureLayout(sub);
+				if(result.y > height)
+					height = result.y;
+				width += result.x;
+			}
+			repr.setSize(new Point((width == 0) ? 1 : width, height + 1));
+			return repr.subSize;
+		default:
+			return new Point(width, height);
+		}
+	}
+	
+	@Override
+	public GraphicalRepresentationElement getRepresentation()
+	{
+		return (GraphicalRepresentationElement) theRepresentation;
+	}
+	
+	@Override
+	public GCanvas displayRepresentation()
+	{
+		// ((GraphicalRepresentationElement)theRepresentation).gelement.setMoveTo(new Point2D.Float(20, 10));
+		
+		return canvas;
+	}
+}
---
+++ src/net/xqhs/graphs/representation/graphical/GraphicalRepresentationElement.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (C) 2013 Andrei Olaru.
+ * 
+ * This file is part of net.xqhs.Graphs.
+ * 
+ * net.xqhs.Graphs is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * net.xqhs.Graphs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with net.xqhs.Graphs.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.xqhs.graphs.representation.graphical;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.xqhs.graphical.GConnector;
+import net.xqhs.graphical.GContainer;
+import net.xqhs.graphical.GElement;
+import net.xqhs.graphical.GElement.ReferenceType;
+import net.xqhs.graphical.GLabel;
+import net.xqhs.graphs.graph.Edge;
+import net.xqhs.graphs.graph.Node;
+import net.xqhs.graphs.representation.GraphRepresentation;
+import net.xqhs.graphs.representation.RepresentationElement;
+import net.xqhs.graphs.representation.VisualizableGraphComponent;
+
+public class GraphicalRepresentationElement extends RepresentationElement
+{
+	enum Type {
+		NODE, EDGE, ELEMENT_CONTAINER
+	}
+	
+	enum EdgeType {
+		CHILDLINK, SIDELINK, EXTLINK, FORELINK, BACKLINK
+	}
+	
+	Type									type;
+	
+	GElement								gelement		= null;
+	GLabel									glabel			= null;
+	Point									gridPos			= null; // not applicable for edges
+	Point									subSize			= null; // not applicable for edges
+	String									label			= "";
+	float									widthFactor		= 1;
+	float									heightFactor	= 1;
+	
+	// GraphComponent peer = null;
+	List<GraphicalRepresentationElement>	connected		= null;
+	EdgeType								edgeType		= null;
+	
+	public GraphicalRepresentationElement(GraphRepresentation root, VisualizableGraphComponent component,
+			Type elementType)
+	{
+		super(root, component);
+		
+		this.type = elementType;
+		connected = new LinkedList<GraphicalRepresentationElement>();
+		
+		switch(elementType)
+		{
+		case ELEMENT_CONTAINER:
+			gelement = new GContainer().setReferenceType(ReferenceType.TOPLEFT);
+			break;
+		case EDGE:
+			gelement = new GConnector();
+			glabel = new GLabel().setText(((Edge) this.getRepresentedComponent()).getLabel()).setParent(gelement);
+			break;
+		case NODE:
+			gelement = new GElement();
+			glabel = new GLabel().setText(((Node) this.getRepresentedComponent()).getLabel()).setParent(gelement);
+			break;
+		default:
+			gelement = new GElement();
+			break;
+		}
+		gelement.setRepresented(getRepresentedComponent());
+	}
+	
+	public GraphicalRepresentationElement setEdge(EdgeType type, GraphicalRepresentationElement from,
+			GraphicalRepresentationElement to)
+	{
+		if(this.type != Type.EDGE)
+			throw new IllegalArgumentException("function is only available for edges");
+		this.connected.add(from);
+		this.connected.add(to);
+		this.edgeType = type;
+		return this;
+	}
+	
+	public GraphicalRepresentationElement setSize(Point size)
+	{
+		this.subSize = size;
+		return this;
+	}
+	
+	public GraphicalRepresentationElement positionInGrid(Point position, float widthFactor, float heightFactor)
+	{
+		this.gridPos = new Point(position);
+		this.widthFactor = widthFactor;
+		this.heightFactor = heightFactor;
+		
+		gelement.setMoveTo(new Point2D.Float(gridPos.x * widthFactor, gridPos.y * heightFactor));
+		return this;
+	}
+}
